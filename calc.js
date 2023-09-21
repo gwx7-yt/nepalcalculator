30ae6db1a9bedc7fc391a4208ef47187c43f92a9
gwx7-yt <143927133+gwx7-yt@users.noreply.github.com>
2023-09-21 21:00:59 +0545

    Easter egg 1 add

--- calc.js
+++ calc.js
@@ -1,12 +1,11 @@
-
-
-
 let input = document.getElementById("input");
 let equal = document.getElementById("equal");
 let clear = document.getElementById("clear");
 let erase = document.getElementById("erase");
 
-let currentInput = ""; 
+let currentInput = "";
+
+// Function to convert Nepali numerals to Arabic numerals
 function convertToArabicNumerals(nepaliNumber) {
   const nepaliNumerals = ["०", "१", "२", "३", "४", "५", "६", "७", "८", "९"];
   let arabicNumber = "";
@@ -19,6 +18,8 @@ function convertToArabicNumerals(nepaliNumber) {
   }
   return arabicNumber;
 }
+
+// Function to convert Arabic numerals to Nepali numerals
 function convertToNepaliNumerals(arabicNumber) {
   const nepaliNumerals = ["०", "१", "२", "३", "४", "५", "६", "७", "८", "९"];
   let nepaliNumber = "";
@@ -31,16 +32,26 @@ function convertToNepaliNumerals(arabicNumber) {
   }
   return nepaliNumber;
 }
+
+// Function to perform the calculation
 function performOperation(inputValue) {
   let arabicInput = convertToArabicNumerals(inputValue);
 
   try {
     let result = eval(arabicInput);
+
+    if (arabicInput === "77+88") {
+      // Easter egg condition: If the input is "77+88", redirect to a YouTube link
+      window.location.href = "https://youtu.be/xvFZjo5PgG0?si=Z1iO9Ol3FuU6lkXo";
+      return "तपाईंले चाल पत्ता लगाउनुभयो!";
+    }
+
     return result;
   } catch (err) {
     return "Error";
   }
 }
+    
 
 document.querySelectorAll(".input-button").forEach((button) => {
   button.addEventListener("click", () => {
@@ -48,7 +59,6 @@ document.querySelectorAll(".input-button").forEach((button) => {
     input.value = convertToNepaliNumerals(currentInput);
   });
 });
-
 
 equal.addEventListener("click", () => {
   let result = performOperation(currentInput);
@@ -58,7 +68,7 @@ equal.addEventListener("click", () => {
     setTimeout(() => {
       input.value = "";
     }, 1000);
-  } else {
+  } else if (result !== undefined) {
     input.value = convertToNepaliNumerals(result.toString());
     currentInput = result.toString();
   }
@@ -72,20 +82,21 @@ clear.addEventListener("click", () => {
 erase.addEventListener("click", () => {
   currentInput = currentInput.substr(0, currentInput.length - 1);
   input.value = convertToNepaliNumerals(currentInput);
-  });
-  const darkModeToggle = document.getElementById('dark-mode-toggle');
-  const body = document.body;
-  const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+});
 
-  if (prefersDarkMode) {
+const darkModeToggle = document.getElementById('dark-mode-toggle');
+const body = document.body;
+const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+if (prefersDarkMode) {
+  body.classList.add('dark-mode');
+  darkModeToggle.checked = true;
+}
+
+darkModeToggle.addEventListener('change', () => {
+  if (darkModeToggle.checked) {
     body.classList.add('dark-mode');
-    darkModeToggle.checked = true;
+  } else {
+    body.classList.remove('dark-mode');
   }
-
-  darkModeToggle.addEventListener('change', () => {
-    if (darkModeToggle.checked) {
-      body.classList.add('dark-mode');
-    } else {
-      body.classList.remove('dark-mode');
-    }
-  });
+});
