3eb58d4c7e787c9ebc126c1df227766166af3142
gwx7-yt <143927133+gwx7-yt@users.noreply.github.com>
2023-09-30 21:00:59 +0545

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('my-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/calc.html',
+        '/path/to/your/assets',
+        
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
